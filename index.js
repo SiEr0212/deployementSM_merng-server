9e62433065da2b9a309c7ed31f98dd55410b30e8
SiEr0212 <simonertel@gmx.de>
2021-04-25 23:53:41 +0200

    fixed PORT in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MONGODB } = require("./config");
 
 const cors = require("cors");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const pubsub = new PubSub();
 
